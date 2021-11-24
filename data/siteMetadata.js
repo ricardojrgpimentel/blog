2aee905203feb1f41a30048612f2fc74a92f52d1
Ricardo Pimentel <ricardojrgpimentel@gmail.com>
2021-11-24 14:17:57 +0000

    added new post added comment system small changes to TOCInline

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -30,7 +30,7 @@ const siteMetadata = {
   comment: {
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: '', // supported providers: giscus, utterances, disqus
+    provider: 'disqus', // supported providers: giscus, utterances, disqus
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
